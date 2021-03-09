4451d527bec4acedfb2054315d1e25612c1d3918
Daysha Jones <daysha.vu@icloud.com>
2021-03-09 00:35:12 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,7 @@
 def valid_move?(board, index)
-  if index.between?(0,8) && position_taken?(board, index) == 
+  if index.between?(0,8) && position_taken?(board, index) == false 
+    return true
+    
 end
 
 def position_taken?(board, index)
